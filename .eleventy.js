20441643f163412ac9824035d97094c6f65bc221
zulvkr <ivan.zulf.ikar@gmail.com>
2021-06-15 22:36:09 +0700

    add image passthrough

--- .eleventy.js
+++ .eleventy.js
@@ -2,8 +2,14 @@ const env = process.env
 
 module.exports = function (eleventyConfig) {
   if (env.NODE_ENV !== 'production') {
-    eleventyConfig.addPassthroughCopy({ 'src/temp/main.css': 'main.css' })
+    eleventyConfig.addPassthroughCopy({
+      'src/temp/main.css': 'main.css',
+      'src/assets/red.css': 'red.css'
+    })
   }
+  eleventyConfig.addPassthroughCopy({
+    'src/image/': 'image'
+  })
   return {
     dir: {
       input: 'src'
