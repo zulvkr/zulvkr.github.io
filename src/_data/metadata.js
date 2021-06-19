fdcf15b55c68553463e7bbe39eb5e75a037a61d9
zulvkr <ivan.zulf.ikar@gmail.com>
2021-06-19 19:38:50 +0700

    Create metadata

---
+++ src/_data/metadata.js
@@ -0,0 +1,9 @@
+module.exports = function () {
+  return {
+    title: 'zul',
+    description: `
+      Array of writings I want to share with the world.
+      I haven't write much though.
+      `
+  }
+}
