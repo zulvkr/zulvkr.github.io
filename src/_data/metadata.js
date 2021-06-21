d0c9843c8a162a45fcee134549d14b0efb5c68e8
zulvkr <ivan.zulf.ikar@gmail.com>
2021-06-21 20:07:49 +0700

    edit home styling

--- src/_data/metadata.js
+++ src/_data/metadata.js
@@ -1,6 +1,6 @@
 module.exports = function () {
   return {
-    title: 'zul',
+    title: 'zulvkr',
     description: `
       Array of writings I want to share with the world.
       I haven't write much though.
