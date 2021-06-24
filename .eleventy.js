70a6338d33dee874c035d760e508816a75c41312
zulvkr <ivan.zulf.ikar@gmail.com>
2021-06-24 08:18:30 +0700

    add minifier, pass icon to _site

--- .eleventy.js
+++ .eleventy.js
@@ -2,21 +2,33 @@ const env = process.env
 const { format } = require('date-fns')
 const { inspect } = require('util')
 const eleventyNavigationPlugin = require('@11ty/eleventy-navigation')
+const { minify } = require('html-minifier-terser')
 
 module.exports = config => {
   config.addPassthroughCopy({
-    'src/image/': 'image'
+    'src/image/': 'image',
+    'src/assets/favicon/*': '/',
+    'src/manifest.webmanifest': 'manifest.webmanifest'
   })
 
-  config.addFilter('toReadableDate', date =>
-    format(date, 'MMMM do, yyyy')
-  )
+  config.addFilter('toReadableDate', date => format(date, 'MMMM do, yyyy'))
 
   config.addFilter('debug', content => `<pre>${inspect(content)}</pre>`)
 
   config.addPlugin(eleventyNavigationPlugin)
 
-  config.setUseGitIgnore(false);
+  config.setUseGitIgnore(false)
+
+  config.addTransform('htmlmin', function (content, outputPath) {
+    if (outputPath && outputPath.endsWith('.html')) {
+      return minified = minify(content, {
+        useShortDoctype: true,
+        removeComments: true,
+        collapseWhitespace: true
+      })
+    }
+    return content
+  })
 
   const configObject = {
     dir: {
