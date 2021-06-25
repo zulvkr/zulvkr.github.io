f8993477b6bd8af03ff05e7d1f58826937f2c69d
zulvkr <ivan.zulf.ikar@gmail.com>
2021-06-25 21:00:23 +0700

    tailwind reconfig, add font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,51 @@
+const sans = [
+  'ui-sans-serif',
+  'system-ui',
+  '-apple-system',
+  'BlinkMacSystemFont',
+  'Segoe UI',
+  'Roboto',
+  'Helvetica Neue',
+  'Arial',
+  'Noto Sans',
+  'sans-serif',
+  'Apple Color Emoji',
+  'Segoe UI Emoji',
+  'Segoe UI Symbol',
+  'Noto Color Emoji'
+]
+
+const serif = [
+  'ui-serif',
+  'Georgia',
+  'Cambria',
+  'Times New Roman',
+  'Times',
+  'serif'
+]
+
+const mono = [
+  'ui-monospace',
+  'SFMono-Regular',
+  'Menlo',
+  'Monaco',
+  'Consolas',
+  'Liberation Mono',
+  'Courier New',
+  'monospace'
+]
+
 module.exports = {
   purge: ['./src/**/*.html', './src/**/*.js'],
   darkMode: 'media', // or 'media' or 'class'
   theme: {
-    extend: {}
+    extend: {},
+    fontFamily: {
+      sans,
+      serif,
+      mono,
+      display: ['Ubuntu Condensed', ...sans]
+    }
   },
   variants: {
     extend: {}
