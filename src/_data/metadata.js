dd14916c5bcd913eb39827e5f3612fe4351b6402
Ivan Zulfikar <ivan.zulf.ikar@gmail.com>
2021-12-31 19:13:58 +0700

    chore: fix typo

--- src/_data/metadata.js
+++ src/_data/metadata.js
@@ -3,7 +3,7 @@ module.exports = function () {
     title: 'zulvkr',
     description: `
     Hi! I'm Zul, a muslim Indonesian, living in Java Island.
-    I work as web developer. I mostly code in JS, but prefer Python on backend.
+    I work as web developer, mostly code in JavaScript.
     `,
     github: 'https://github.com/zulvkr',
   }
