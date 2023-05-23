2255cabc8ad685eb875ffee849e4ad245e0d86fd
Ivan Zulfikar <ivan.zulf.ikar@gmail.com>
2023-05-23 23:48:58 +0700

    add gh workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,6 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://ivanzulfikar.com',
   integrations: [mdx(), sitemap(), tailwind(), react()]
 });
